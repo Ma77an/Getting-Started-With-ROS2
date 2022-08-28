793c6a3f9cf84557151c8e4807ee196b361774ed
Matan Gabso <matang99@gmail.com>
2022-08-28 21:16:30 +0300

    idk some playing with symlink for the py_node and some turtlesim testing

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -7,7 +7,7 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
         self.counter_ = 0
-        self.get_logger().info("Hello ROS2")
+        self.get_logger().info("Hello ROS2!!!!!!!!!")
         self.create_timer(0.5, self.timer_callback)
 
     def timer_callback(self):
@@ -24,20 +24,3 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
-
-    #!/usr/bin/env python3
-    import rclpy
-    from rclpy.node import Node
-
-    class MyCustomNode(Node):  # MODIFY NAME
-        def __init__(self):
-            super().__init__("node_name")  # MODIFY NAME
-
-    def main(args=None):
-        rclpy.init(args=args)
-        node = MyCustomNode()  # MODIFY NAME
-        rclpy.spin(node)
-        rclpy.shutdown()
-
-    if __name__ == "__main__":
-        main()
