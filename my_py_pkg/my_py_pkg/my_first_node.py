a4e47eed714ffa40a80ff9a92be23ef1af414071
Matan Gabso <matang99@gmail.com>
2022-08-28 16:33:06 +0300

    init

---
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self.counter_ = 0
+        self.get_logger().info("Hello ROS2")
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("Hello " + str(self.counter_))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
+    #!/usr/bin/env python3
+    import rclpy
+    from rclpy.node import Node
+
+    class MyCustomNode(Node):  # MODIFY NAME
+        def __init__(self):
+            super().__init__("node_name")  # MODIFY NAME
+
+    def main(args=None):
+        rclpy.init(args=args)
+        node = MyCustomNode()  # MODIFY NAME
+        rclpy.spin(node)
+        rclpy.shutdown()
+
+    if __name__ == "__main__":
+        main()
